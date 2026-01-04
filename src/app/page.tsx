f92c7d8114b2fb73c35aaa90e7b431560274239b
Jeet <jeetnath2110@gmail.com>
2026-01-04 13:10:41 +0530

    Add button for navigation on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import GuestLayout from "@/components/layouts/guestLayout";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      
-    </>
+    <GuestLayout>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <Link href="/pulse" className="text-blue-500 underline">
+          <Button variant="secondary">
+            Go to Pulse Page
+          </Button>
+        </Link>
+      </div>
+    </GuestLayout>
   );
 }
---
+++ src/components/layouts/guestLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GuestLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default GuestLayout
